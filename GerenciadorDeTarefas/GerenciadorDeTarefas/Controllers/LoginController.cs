99acfd46472558ba34a34c606290e27617e26ab3
EngAdriano <adriano.liukin@alu.ufc.br>
2021-06-04 11:35:59 -0300

    Primeira parte projeto API C# Dot Net Core

---
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using GerenciadorDeTarefas.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas.Controllers
+{
+    [ApiController]                  //Anotação para as ApiControllers
+    [Route("/api/[Controller]")]     //Registrar a Rota da Api. É interessante sempre separa as rotas da API das rotas de Site
+    public class LoginController : ControllerBase
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost] //Post - Método padrão para enviar dados
+        public IActionResult EfetuarLogin([FromBody] LoginRequisicaoDto requisicao)
+        {
+            try
+            {
+                if(requisicao == null || requisicao.Login == null || requisicao.Senha == null)
+                {
+                    return BadRequest(new ErroRespostaDto()
+                    {
+                        Status = StatusCodes.Status400BadRequest,
+                        Erro = "Parâmetros de entrada inválidos"
+                    });
+                }
+
+                return Ok("Usuário autenticado com sucesso");
+            }
+            catch (Exception excecao)
+            {
+                _logger.LogError($"Ocorreu erro ao efetuar login: {excecao.Message}", excecao, requisicao);
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErroRespostaDto()
+                {
+                    Status = StatusCodes.Status500InternalServerError,
+                    Erro = "Ocorreu erro ao efetuar login, tente novamente!"
+                });
+            }
+        }
+    }
+}
